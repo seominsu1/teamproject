ac26805e0c674a4565bdbd2ef87307dc815b596f
dlseldk21 <dlseldk21@naver.com>
2022-01-20 12:11:05 +0900

    Initial Commit

--- app01/views.py
+++ app01/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from restaurant.forms import ResinputForm
-
+from restaurant.models import restaurant
+from django.db.models import Q
 # Create your views here.
 def home(request):
     return render(request,'test.html')
@@ -28,3 +29,15 @@ def rest_input(request):
             return redirect('/staff/restInput')
         print('유효하지 않음')
 
+#음식점 세부정보 사이트
+def rest_detail(request,bid):
+    rest=restaurant.objects.get(Q(id=bid))
+
+    # 음식점 세부 정보+지도+메뉴 띄우는 건 get 방식(음식점id만 있으면 출력)
+    if request.method=='GET':
+        rest_info=ResinputForm(instance=rest)
+        return render(request,'rest_detail.html',{'rest_info':rest_info})
+    # 같은 화면 내에 댓글+평점 창도 있으니깐, 그때는 post
+    # 기존에 생성되어있는 댓글 목록 보여져야 됨 (음식점id 외래키로 하는 review 모두 출력)
+    else:
+        return render(request,'test.html')
