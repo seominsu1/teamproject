c3acf45ed385e4a903da5e6a697c1e0274220e8b
seominsu1 <sms897@naver.com>
2022-01-20 13:35:18 +0900

    commit

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,17 +1,3 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from restaurant.forms import BoardForm
-
-
-def register(request):
-    if request.method == 'GET' :
-        boardForm = BoardForm()
-        return render(request, 'register.html', {'boardForm':boardForm})
-    elif request.method == 'POST':
-        boardForm = BoardForm(request.POST)
-        if boardForm.is_valid():
-            board = boardForm.save(commit=False)
-            board.save()
-            return redirect('/register')
-
