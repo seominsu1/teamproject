c6039f6c9be9097f710d3b8536ad8a8eaecc0252
seominsu1 <sms897@naver.com>
2022-01-20 17:08:36 +0900

    commit

--- config/settings.py
+++ config/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
+#import pymysql
+#pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,7 +83,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'tp',
-        'USER': 'user3',
+        'USER': 'user2',
         'PASSWORD': 'qwer1234',
         'HOST': '172.30.1.109',
         'PORT': '3306',
