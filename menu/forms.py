39b2e886037eeddc5eca23047b1e8ce967c408ee
dlseldk21 <dlseldk21@naver.com>
2022-01-20 16:36:04 +0900

    Initial Commit

---
+++ menu/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from menu.models import Menu
+
+#음식점 DB POST 형식으로 전달하기 위한 형식
+class MenuinputForm(forms.ModelForm):
+    class Meta:
+        model=Menu
+        fields=('name','price','img_url','ingredient')
