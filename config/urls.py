ad3b5beddbe12d588c213825c2c3e36207f538e1
slinfo <qwe7791@naver.com>
2022-01-21 13:33:29 +0900

    유저 다함

--- config/urls.py
+++ config/urls.py
@@ -19,12 +19,25 @@ from django.urls import path
 
 import app01.views
 import restaurant.views
+import users.views
 import menu.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('test', restaurant.views.home),
 
-    path('main', app01.views.main_page),
+    path('users/test', users.views.home),
+    path('usersbase', users.views.base),
+
+    path('users/signup', users.views.signup),  # 가입
+
+    path('users/login', users.views.userlogin),  # 로그인
+    path('users/logout', users.views.userlogout),  # 로그아웃
+
+    path('users/pwchange', users.views.userpwchange),  # 비밀번호 변경
+    path('users/delete', users.views.userdelete),  # 회원탈퇴
+
+    path('main', users.views.main_page),
+]
 
 
